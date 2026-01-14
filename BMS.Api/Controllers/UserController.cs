0c13074ddb933117cea1fabc3b7190b107cb7de6
Yogesh <ymrathod1412@gmail.com>
2026-01-14 18:17:27 +0530

    add user api with get endpoint

---
+++ BMS.Api/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
