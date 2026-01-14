54333675bf89a38637868862856d4cb380405e30
Yogesh <ymrathod1412@gmail.com>
2026-01-14 17:30:47 +0530

    Add user dto for api and mvc communication

---
+++ BMS.Core/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BMS.Core.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
+    }
+}
