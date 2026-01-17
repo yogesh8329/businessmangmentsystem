2c460bbc22b9c347ba90962ce80348bd27b68f22
Yogesh <ymrathod1412@gmail.com>
2026-01-17 13:50:26 +0530

    feat:add createdorderdto and orderdto

---
+++ BMS.Core/DTOs/CreateOrderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BMS.Core.DTOs
+{
+    public class CreateOrderDto
+    {
+        public int UserId { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ BMS.Core/DTOs/OrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BMS.Core.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
